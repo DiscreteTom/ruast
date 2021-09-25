af0938c5bfbb132b89fe9544b50165677f4b77f2
DiscreteTom <discrete_tom@outlook.com>
2021-09-25 15:47:26 +0800

    update model, use Box<dyn Error>

--- src/model.rs
+++ src/model.rs
@@ -1,8 +1,8 @@
-use std::{io::Error, sync::Arc, sync::Weak, time::SystemTime};
+use std::{error::Error, sync::Arc, sync::Weak, time::SystemTime};
 
 pub trait Peer<'a> {
-    fn write(&self, data: &Arc<&[u8]>) -> Result<(), Error>;
-    fn close(&self) -> Result<(), Error>;
+    fn write(&self, data: &Arc<&[u8]>) -> Result<(), Box<dyn Error>>;
+    fn close(&self) -> Result<(), Box<dyn Error>>;
     fn start(&self);
     fn set_id(&self, id: i32);
     fn id(&self) -> i32;
@@ -18,7 +18,7 @@ pub struct PeerMsg<'a> {
 
 pub trait GameServer {
     fn add_peer(&self, peer: dyn Peer) -> i32;
-    fn remove_peer(&self, id: i32) -> Result<(), Error>;
+    fn remove_peer(&self, id: i32) -> Result<(), Box<dyn Error>>;
     fn append_peer_msg(&self, peer: dyn Peer, data: [u8]);
     fn stop(&self);
     fn for_each_peer(&self, f: dyn Fn(dyn Peer));
