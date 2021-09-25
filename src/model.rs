eacec159a51639b7ef033ab91ab22521b2d34c49
DiscreteTom <discrete_tom@outlook.com>
2021-09-25 15:53:18 +0800

    update model

--- src/model.rs
+++ src/model.rs
@@ -4,7 +4,7 @@ pub trait Peer<'a> {
     fn write(&self, data: &Arc<&[u8]>) -> Result<(), Box<dyn Error>>;
     fn close(&self) -> Result<(), Box<dyn Error>>;
     fn start(&self);
-    fn set_id(&self, id: i32);
+    fn activate(&self, id: i32);
     fn id(&self) -> i32;
     fn set_tag(&self, tag: &str);
     fn tag(&self) -> &'a str;
@@ -16,10 +16,9 @@ pub struct PeerMsg<'a> {
     pub time: SystemTime,
 }
 
-pub trait GameServer {
-    fn add_peer(&self, peer: dyn Peer) -> i32;
+pub trait GameServer<'a> {
+    fn add_peer(&self, peer: impl Peer<'a>) -> i32;
     fn remove_peer(&self, id: i32) -> Result<(), Box<dyn Error>>;
-    fn append_peer_msg(&self, peer: dyn Peer, data: [u8]);
     fn stop(&self);
     fn for_each_peer(&self, f: dyn Fn(dyn Peer));
     fn peer(&self, id: i32) -> Option<Weak<dyn Peer>>;
