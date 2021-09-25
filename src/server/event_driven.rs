e45cc153e19250a8398af59d8fe587c4aafa0e9c
DiscreteTom <discrete_tom@outlook.com>
2021-09-26 03:13:11 +0800

    update event_driven

--- src/server/event_driven.rs
+++ src/server/event_driven.rs
@@ -3,21 +3,16 @@ use std::{
     error::Error,
     sync::{
         mpsc::{self, Receiver, Sender},
-        Mutex, Weak,
+        Mutex,
     },
 };
 
-use crate::model::{GameServer, Peer, PeerMsg};
-
-pub enum ServerEvent<'a> {
-    Msg(PeerMsg<'a>),
-    Stop,
-}
+use crate::model::{GameServer, Peer, PeerMsg, ServerError, ServerEvent};
 
 pub struct Server<'a> {
     name: String,
     peers: Mutex<HashMap<i32, Box<dyn Peer<'a>>>>,
-    on_peer_msg_handler: dyn Fn(PeerMsg),
+    on_peer_msg_handler: Box<dyn Fn(PeerMsg)>,
     event_sender: Sender<ServerEvent<'a>>,
     event_receiver: Receiver<ServerEvent<'a>>,
 }
@@ -28,7 +23,7 @@ impl<'a> Server<'a> {
         Server {
             name: String::from("EventDrivenServer"),
             peers: Mutex::new(HashMap::new()),
-            on_peer_msg_handler: || {},
+            on_peer_msg_handler: Box::new(|_| {}),
             event_sender,
             event_receiver,
         }
@@ -38,7 +33,7 @@ impl<'a> Server<'a> {
         // process peer message and wait for stop
         loop {
             match self.event_receiver.recv().unwrap() {
-                ServerEvent::Msg(msg) => self.on_peer_msg_handler.call(msg),
+                ServerEvent::Msg(msg) => (self.on_peer_msg_handler)(msg),
                 ServerEvent::Stop => break,
             }
         }
@@ -46,33 +41,33 @@ impl<'a> Server<'a> {
         // close all peers
         self.peers.lock().unwrap().iter().map(|(_, peer)| {
             peer.close();
-        })
+        });
     }
 
-    pub fn on_peer_msg(&self, f: dyn Fn(PeerMsg)) -> Self {
+    pub fn on_peer_msg(&mut self, f: Box<dyn Fn(PeerMsg)>) -> &Self {
         self.on_peer_msg_handler = f;
         self
     }
 }
 
-impl<'a> GameServer for Server<'a> {
-    fn add_peer(&self, peer: Box<dyn Peer>) -> i32 {
-        let peers = self.peers.lock().unwrap();
+impl<'a> GameServer<'a> for Server<'a> {
+    fn add_peer(&self, peer: Box<dyn Peer<'a>>) -> i32 {
+        let mut peers = self.peers.lock().unwrap();
         let new_peer_id = match peers.keys().max() {
             Some(max) => max + 1,
             None => 0,
         };
-        peers.insert(new_peer_id, peer);
         peer.activate(new_peer_id, self.event_sender.clone());
         peer.start();
+        peers.insert(new_peer_id, peer);
         new_peer_id
     }
 
     fn remove_peer(&self, id: i32) -> Result<(), Box<dyn Error>> {
-        let peers = self.peers.lock().unwrap();
+        let mut peers = self.peers.lock().unwrap();
         match peers.remove(&id) {
             Some(_) => Ok(()),
-            None => Err("peer not exist"),
+            None => Err(Box::new(ServerError::PeerNotExist(id))),
         }
     }
 
@@ -80,11 +75,17 @@ impl<'a> GameServer for Server<'a> {
         self.event_sender.send(ServerEvent::Stop);
     }
 
-    fn for_each_peer(&self, f: dyn Fn(dyn Peer)) {
-        self.peers.lock().unwrap().iter().map(f)
+    fn for_each_peer(&self, f: Box<dyn FnMut((&i32, &Box<dyn Peer>))>) {
+        self.peers.lock().unwrap().iter().map(f);
     }
 
-    fn peer(&self, id: i32) -> Option<Weak<dyn Peer>> {
-        self.peers.lock().unwrap().get(&id)
+    fn apply_to(&self, id: i32, mut f: Box<dyn FnMut(&Box<dyn Peer>)>) -> Result<(), Box<dyn Error>> {
+        match self.peers.lock().unwrap().get(&id) {
+            Some(peer) => {
+                f(peer);
+                Ok(())
+            }
+            None => Err(Box::new(ServerError::PeerNotExist(id))),
+        }
     }
 }
