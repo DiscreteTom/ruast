86ba7b4dae3c9aca7274fc4e60c6504b75944744
DiscreteTom <discrete_tom@outlook.com>
2021-09-26 22:19:48 +0800

    update model

--- src/model.rs
+++ src/model.rs
@@ -6,24 +6,24 @@ use std::{
   time::SystemTime,
 };
 
-pub trait Peer<'a> {
-  fn write(&mut self, data: Arc<&[u8]>) -> Result<(), Box<dyn Error>>;
+pub trait Peer {
+  fn write(&mut self, data: Arc<[u8]>) -> Result<(), Box<dyn Error>>;
   fn close(&mut self) -> Result<(), Box<dyn Error>>;
   fn start(&mut self);
-  fn activate(&mut self, id: i32, msg_sender: Sender<ServerEvent<'a>>);
+  fn activate(&mut self, id: i32, msg_sender: Sender<ServerEvent>);
   fn id(&self) -> i32;
   fn set_tag(&mut self, tag: &str);
   fn tag(&self) -> &str;
 }
 
-pub struct PeerMsg<'a> {
-  pub peer: Weak<dyn Peer<'a>>,
-  pub data: Arc<&'a [u8]>,
+pub struct PeerMsg {
+  pub peer: Weak<dyn Peer>,
+  pub data: Arc<[u8]>,
   pub time: SystemTime,
 }
 
-pub enum ServerEvent<'a> {
-  Msg(PeerMsg<'a>),
+pub enum ServerEvent {
+  Msg(PeerMsg),
   Stop,
 }
 
@@ -34,17 +34,16 @@ pub enum ServerError {
 
 impl fmt::Display for ServerError {
   fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-    // match *self {
-    //     ServerError::PeerNotExist(id) => write!(f, "peer not exist, id={}", id),
-    // }
-    write!(f, "123")
+    match *self {
+      ServerError::PeerNotExist(id) => write!(f, "peer not exist, id={}", id),
+    }
   }
 }
 
 impl Error for ServerError {}
 
-pub trait GameServer<'a> {
-  fn add_peer(&self, peer: Box<dyn Peer<'a>>) -> i32;
+pub trait GameServer {
+  fn add_peer(&self, peer: Box<dyn Peer>) -> i32;
   fn remove_peer(&self, id: i32) -> Result<(), Box<dyn Error>>;
   fn stop(&self);
   fn for_each_peer(&self, f: Box<dyn FnMut(&Box<dyn Peer>)>);
