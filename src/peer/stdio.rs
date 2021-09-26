c9afdc71b2619fdd315e5f14c6a3ee25e9b6f7be
DiscreteTom <discrete_tom@outlook.com>
2021-09-26 22:33:59 +0800

    update stdio peer

--- src/peer/stdio.rs
+++ src/peer/stdio.rs
@@ -1,42 +1,66 @@
-use std::{error::Error, io, sync::{Arc, mpsc::Sender}};
+use std::{
+  error::Error,
+  io,
+  sync::{mpsc::Sender, Arc},
+  time::SystemTime,
+};
 
 use crate::model::{Peer, PeerMsg, ServerEvent};
 
-pub struct StdioPeer<'a>{
-  msg_sender: Option<Sender<PeerMsg<'a>>>,
+pub struct StdioPeer {
+  msg_sender: Option<Sender<ServerEvent>>,
   tag: String,
   id: Option<i32>,
 }
 
-impl<'a> StdioPeer<'a>{
-  fn new()->StdioPeer<'a>{
-    StdioPeer{
+impl StdioPeer {
+  pub fn new() -> StdioPeer {
+    StdioPeer {
       msg_sender: None,
       tag: String::from("stdio"),
-      id: None
+      id: None,
     }
   }
 }
 
-impl<'a> Peer<'a> for StdioPeer<'a>{
-  fn write(&self, data: &Arc<&[u8]>) -> Result<(), Box<dyn Error>>{
-    println!("{}", String::from_utf8_lossy(data));
+impl Peer for StdioPeer {
+  fn write(&mut self, data: Arc<[u8]>) -> Result<(), Box<dyn Error>> {
+    println!("{}", String::from_utf8_lossy(&data));
     Ok(())
   }
-  fn close(&self) -> Result<(), Box<dyn Error>>{
+  fn close(&mut self) -> Result<(), Box<dyn Error>> {
     Ok(())
   }
-  fn start(&self){
-    let mut line = String::new();
-    io::stdin().read_line(&mut line)
+  fn start(&mut self) {
+    loop {
+      let mut line = String::new();
+      io::stdin()
+        .read_line(&mut line)
         .expect("Failed to read line");
+      match self.msg_sender {
+        // Some(sender) => sender
+        //   .send(ServerEvent::Msg(PeerMsg {
+        //     peer: self,
+        //     data: Arc::new(line.as_bytes()),
+        //     time: SystemTime::now(),
+        //   }))
+        //   .unwrap(),
+        Some(_)=>(),
+        None => continue,
+      }
+    }
   }
-  fn activate(&self, id: i32, msg_sender: Sender<ServerEvent>){}
-  fn id(&self) -> i32{
+  fn activate(&mut self, id: i32, msg_sender: Sender<ServerEvent>) {
+    self.id = Some(id);
+    self.msg_sender = Some(msg_sender);
+  }
+  fn id(&self) -> i32 {
     self.id.unwrap()
   }
-  fn set_tag(&self, tag: &str){}
-  fn tag(&'a self) -> &'a str{
+  fn set_tag(&mut self, tag: &str) {
+    self.tag = tag.to_string();
+  }
+  fn tag(&self) -> &str {
     &self.tag
   }
 }
