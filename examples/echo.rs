a509b04b69535c3bbd38c6b44a1c49777739752b
DiscreteTom <discrete_tom@outlook.com>
2021-10-04 02:27:29 +0800

    update example/echo using server.write_to

--- examples/echo.rs
+++ examples/echo.rs
@@ -4,10 +4,7 @@ use rua::{model::GameServer, peer::StdioPeer, server::EventDrivenServer};
 
 fn main() -> Result<(), Box<dyn Error>> {
   let mut s = EventDrivenServer::new();
-  s.on_peer_msg(&|msg, s| {
-    s.apply_to(msg.peer_id, |p| p.write(msg.data).unwrap())
-      .unwrap()
-  });
+  s.on_peer_msg(&|msg, s| s.write_to(msg.peer_id, msg.data).unwrap());
   s.add_peer(StdioPeer::new(0, s.tx())).unwrap();
   s.start();
   Ok(())
