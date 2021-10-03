678946ef119ee6e8f285d3e62bac8f0f8f5b3459
DiscreteTom <discrete_tom@outlook.com>
2021-10-04 02:33:23 +0800

    optimize examples/echo

--- examples/echo.rs
+++ examples/echo.rs
@@ -1,11 +1,8 @@
-use std::error::Error;
-
 use rua::{model::GameServer, peer::StdioPeer, server::EventDrivenServer};
 
-fn main() -> Result<(), Box<dyn Error>> {
+fn main() {
   let mut s = EventDrivenServer::new();
   s.on_peer_msg(&|msg, s| s.write_to(msg.peer_id, msg.data).unwrap());
   s.add_peer(StdioPeer::new(0, s.tx())).unwrap();
   s.start();
-  Ok(())
 }
