5f616911a666e1f1ba80b13348ce8fd45d696a0e
DiscreteTom <discrete_tom@outlook.com>
2021-10-04 01:09:40 +0800

    update example/echo to adapt msg_based model

--- examples/echo.rs
+++ examples/echo.rs
@@ -3,16 +3,12 @@ use std::error::Error;
 use rua::{model::GameServer, peer::StdioPeer, server::EventDrivenServer};
 
 fn main() -> Result<(), Box<dyn Error>> {
-  let (mut s, event_sender) = EventDrivenServer::new();
-  s.on_peer_msg(|msg| {
-    (msg.peer.upgrade().unwrap())
-      .lock()
+  let mut s = EventDrivenServer::new();
+  s.on_peer_msg(&|msg, s| {
+    s.apply_to(msg.peer_id, |p| p.write(msg.data).unwrap())
       .unwrap()
-      .write(msg.data.clone())
-      .unwrap();
   });
-  s.add_peer(StdioPeer::new(String::from("stdio"), event_sender.clone())?)
-    .unwrap();
+  s.add_peer(StdioPeer::new(0, s.tx())).unwrap();
   s.start();
   Ok(())
 }
