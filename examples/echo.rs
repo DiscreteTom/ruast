e21c03d0fba5eac3f0fa7c76bc0ea8cb43e17815
DiscreteTom <discrete_tom@outlook.com>
2021-10-04 02:37:20 +0800

    update examples/echo using server.echo

--- examples/echo.rs
+++ examples/echo.rs
@@ -2,7 +2,7 @@ use rua::{model::GameServer, peer::StdioPeer, server::EventDrivenServer};
 
 fn main() {
   let mut s = EventDrivenServer::new();
-  s.on_peer_msg(&|msg, s| s.write_to(msg.peer_id, msg.data).unwrap());
+  s.on_peer_msg(&|msg, s| s.echo(msg).unwrap());
   s.add_peer(StdioPeer::new(0, s.tx())).unwrap();
   s.start();
 }
