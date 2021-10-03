e8fc55022041961922f76b02e298e34dd1e9c17d
DiscreteTom <discrete_tom@outlook.com>
2021-10-04 00:03:16 +0800

    update event_driven to adapt msg_based model

--- src/server/event_driven.rs
+++ src/server/event_driven.rs
@@ -1,9 +1,9 @@
 use std::{
-  collections::HashMap,
+  collections::{hash_map::Entry, HashMap},
   error::Error,
   sync::{
     mpsc::{self, Receiver, Sender},
-    Arc, Mutex,
+    Mutex,
   },
 };
 
@@ -11,25 +11,26 @@ use crate::model::{GameServer, Peer, PeerMsg, ServerError, ServerEvent};
 
 pub struct EventDrivenServer {
   name: String,
-  peers: Mutex<HashMap<String, Arc<Mutex<dyn Peer>>>>,
+  peers: Mutex<HashMap<i32, Box<dyn Peer>>>,
   on_peer_msg_handler: fn(PeerMsg),
-  event_sender: Sender<ServerEvent>,
-  event_receiver: Receiver<ServerEvent>,
+  tx: Sender<ServerEvent>,
+  rx: Receiver<ServerEvent>,
 }
 
 impl EventDrivenServer {
-  pub fn new() -> (EventDrivenServer, Sender<ServerEvent>) {
-    let (event_sender, event_receiver) = mpsc::channel();
-    (
-      EventDrivenServer {
-        name: String::from("EventDrivenServer"),
-        peers: Mutex::new(HashMap::new()),
-        on_peer_msg_handler: |_| {},
-        event_sender: event_sender.clone(),
-        event_receiver,
-      },
-      event_sender,
-    )
+  pub fn new() -> Self {
+    let (tx, rx) = mpsc::channel();
+    EventDrivenServer {
+      name: String::from("EventDrivenServer"),
+      peers: Mutex::new(HashMap::new()),
+      on_peer_msg_handler: |_| {},
+      tx,
+      rx,
+    }
+  }
+
+  pub fn tx(&self) -> Sender<ServerEvent> {
+    self.tx.clone()
   }
 
   pub fn start(&self) {
@@ -37,8 +38,10 @@ impl EventDrivenServer {
 
     // process peer message and wait for stop
     loop {
-      match self.event_receiver.recv().unwrap() {
-        ServerEvent::Msg(msg) => (self.on_peer_msg_handler)(msg),
+      match self.rx.recv().unwrap() {
+        ServerEvent::AddPeer(id, peer) => self.add_peer(id, peer).unwrap(),
+        ServerEvent::RemovePeer(id) => self.remove_peer(id).unwrap(),
+        ServerEvent::PeerMsg(msg) => (self.on_peer_msg_handler)(msg),
         ServerEvent::Stop => break,
       }
     }
@@ -51,45 +54,44 @@ impl EventDrivenServer {
 }
 
 impl GameServer for EventDrivenServer {
-  fn add_peer(&self, peer: Arc<Mutex<dyn Peer>>) -> Result<(), Box<dyn Error>> {
-    let mut peers = self.peers.lock().unwrap();
-    let id = peer.lock().unwrap().id().to_string();
-
-    match peers.contains_key(&id) {
-      true => Err(Box::new(ServerError::PeerAlreadyExist(id))),
-      false => {
-        peers.insert(id, peer);
+  fn add_peer(&self, id: i32, peer: Box<dyn Peer>) -> Result<(), Box<dyn Error>> {
+    match self.peers.lock().unwrap().entry(id) {
+      Entry::Occupied(_) => {
+        Err(Box::new(ServerError::PeerAlreadyExist(id)))
+        // the new peer will drop itself since it's not moved into the HashMap
+      }
+      Entry::Vacant(e) => {
+        e.insert(peer);
         Ok(())
       }
     }
   }
 
-  fn remove_peer(&self, id: &str) -> Result<(), Box<dyn Error>> {
-    let mut peers = self.peers.lock().unwrap();
-    let id = id.to_string();
-
-    match peers.remove(&id) {
-      Some(_) => Ok(()),
+  fn remove_peer(&self, id: i32) -> Result<(), Box<dyn Error>> {
+    match self.peers.lock().unwrap().remove(&id) {
+      Some(_) => {
+        // the target peer will drop itself since it's moved out of the HashMap
+        Ok(())
+      }
       None => Err(Box::new(ServerError::PeerNotExist(id))),
     }
   }
 
   fn stop(&self) {
-    self.event_sender.send(ServerEvent::Stop).unwrap();
+    self.tx.send(ServerEvent::Stop).unwrap();
   }
 
-  fn for_each_peer(&self, f: fn(Arc<Mutex<dyn Peer>>)) {
+  fn for_each_peer(&self, f: fn(&Box<dyn Peer>)) {
     for (_, peer) in self.peers.lock().unwrap().iter() {
-      f(peer.clone())
+      // peer.send(PeerEvent::Apply(f)).unwrap();
+      f(peer)
     }
   }
 
-  fn apply_to(&self, id: &str, f: fn(Arc<Mutex<dyn Peer>>)) -> Result<(), Box<dyn Error>> {
-    let id = id.to_string();
-
+  fn apply_to(&self, id: i32, f: fn(&Box<dyn Peer>)) -> Result<(), Box<dyn Error>> {
     match self.peers.lock().unwrap().get(&id) {
       Some(peer) => {
-        f(peer.clone());
+        f(peer);
         Ok(())
       }
       None => Err(Box::new(ServerError::PeerNotExist(id))),
