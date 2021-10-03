a04ca145fea116a45556d9629986d7a2219f5d8e
DiscreteTom <discrete_tom@outlook.com>
2021-10-04 00:27:21 +0800

    update stdio to adapt msg_based model

--- src/peer/debug/stdio.rs
+++ src/peer/debug/stdio.rs
@@ -1,7 +1,7 @@
 use std::{
   error::Error,
   io,
-  sync::{mpsc::Sender, Arc, Mutex, Weak},
+  sync::{mpsc::Sender, Arc},
   thread,
   time::SystemTime,
 };
@@ -10,19 +10,37 @@ use crate::model::{Peer, PeerMsg, ServerEvent};
 
 pub struct StdioPeer {
   tag: String,
-  id: String,
+  id: i32,
+  server_tx: Sender<ServerEvent>,
 }
 
 impl StdioPeer {
-  pub fn new(
-    id: String,
-    msg_sender: Sender<ServerEvent>,
-  ) -> Result<Arc<Mutex<dyn Peer>>, Box<dyn Error>> {
-    let p = Arc::new(Mutex::new(StdioPeer {
+  pub fn new(id: i32, server_tx: Sender<ServerEvent>) -> Box<dyn Peer> {
+    Box::new(StdioPeer {
       tag: String::from("stdio"),
       id,
-    }));
-    let weak_p = Arc::downgrade(&p) as Weak<Mutex<dyn Peer>>;
+      server_tx,
+    })
+  }
+}
+
+impl Peer for StdioPeer {
+  fn write(&mut self, data: Arc<Vec<u8>>) -> Result<(), Box<dyn Error>> {
+    println!("{}", String::from_utf8_lossy(&data));
+    Ok(())
+  }
+  fn id(&self) -> i32 {
+    self.id
+  }
+  fn set_tag(&mut self, tag: &str) {
+    self.tag = String::from(tag);
+  }
+  fn tag(&self) -> &str {
+    &self.tag
+  }
+  fn start(&mut self) -> Result<(), Box<dyn Error>> {
+    let stx = self.server_tx.clone();
+    let id = self.id;
     thread::spawn(move || {
       loop {
         // read line
@@ -32,31 +50,15 @@ impl StdioPeer {
           .expect("Failed to read line");
 
         // send
-        msg_sender
-          .send(ServerEvent::Msg(PeerMsg {
-            peer: weak_p.clone(),
+        stx
+          .send(ServerEvent::PeerMsg(PeerMsg {
+            peer_id: id,
             data: Arc::new(line.into_bytes()),
             time: SystemTime::now(),
           }))
           .unwrap()
       }
     });
-    Ok(p)
-  }
-}
-
-impl Peer for StdioPeer {
-  fn write(&mut self, data: Arc<Vec<u8>>) -> Result<(), Box<dyn Error>> {
-    println!("{}", String::from_utf8_lossy(&data));
     Ok(())
-  }
-  fn id(&self) -> &str {
-    &self.id
-  }
-  fn set_tag(&mut self, tag: &str) {
-    self.tag = String::from(tag);
-  }
-  fn tag(&self) -> &str {
-    &self.tag
   }
 }
