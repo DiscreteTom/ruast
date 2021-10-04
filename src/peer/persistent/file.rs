40631efbf3db08987b7a95e4c38a08fc7226b5e8
DiscreteTom <discrete_tom@outlook.com>
2021-10-04 10:20:41 +0800

    finish FilePeer

--- src/peer/persistent/file.rs
+++ src/peer/persistent/file.rs
@@ -1,1 +1,46 @@
-pub struct FilePeer {}
+use std::{
+  error::Error,
+  fs::{File, OpenOptions},
+  io::{self, Write},
+};
+
+use crate::model::Peer;
+
+pub struct FilePeer {
+  tag: String,
+  id: i32,
+  file: File,
+}
+
+impl FilePeer {
+  pub fn new(id: i32, filename: &str) -> Result<Box<dyn Peer>, io::Error> {
+    Ok(Box::new(FilePeer {
+      tag: String::from("file"),
+      id,
+      file: OpenOptions::new()
+        .create(true)
+        .write(true)
+        .append(true)
+        .open(filename)?,
+    }))
+  }
+}
+
+impl Peer for FilePeer {
+  fn write(&mut self, data: std::sync::Arc<Vec<u8>>) -> Result<(), Box<dyn Error>> {
+    self.file.write_all(&data)?;
+    Ok(self.file.sync_data()?)
+  }
+  fn id(&self) -> i32 {
+    self.id
+  }
+  fn set_tag(&mut self, tag: &str) {
+    self.tag = String::from(tag);
+  }
+  fn tag(&self) -> &str {
+    &self.tag
+  }
+  fn start(&mut self) -> Result<(), Box<dyn Error>> {
+    Ok(())
+  }
+}
