a0f425b400524c97bb16d3918438851631060c59
DiscreteTom <discrete_tom@outlook.com>
2021-10-04 14:57:49 +0800

    add examples/graceful-shutdown

---
+++ examples/graceful-shutdown.rs
@@ -0,0 +1,16 @@
+use ctrlc;
+use rua::{
+  model::{GameServer, ServerEvent},
+  peer::StdioPeer,
+  server::EventDrivenServer,
+};
+
+fn main() {
+  let mut s = EventDrivenServer::new();
+  s.on_peer_msg(&|msg, s| s.echo(msg).unwrap());
+  s.add_peer(StdioPeer::new(0, s.tx())).unwrap();
+
+  let tx = s.tx();
+  ctrlc::set_handler(move || tx.send(ServerEvent::Stop).unwrap()).unwrap();
+  s.start();
+}
