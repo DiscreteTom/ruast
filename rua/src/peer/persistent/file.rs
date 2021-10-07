2f39be5c0a70c1fa5d0df26b37b64a376437e748
DiscreteTom <discrete_tom@outlook.com>
2021-10-07 21:44:32 +0800

    apply bytes to FilePeer

--- rua/src/peer/persistent/file.rs
+++ rua/src/peer/persistent/file.rs
@@ -1,14 +1,17 @@
 use std::{
+  cell::RefCell,
   fs::{File, OpenOptions},
   io::Write,
 };
 
-use crate::model::{Data, Peer, Result};
+use bytes::Bytes;
+
+use crate::model::{Peer, Result};
 
 pub struct FilePeer {
   tag: String,
   id: i32,
-  file: File,
+  file: RefCell<File>,
 }
 
 impl FilePeer {
@@ -16,19 +19,21 @@ impl FilePeer {
     Ok(Box::new(FilePeer {
       tag: String::from("file"),
       id,
-      file: OpenOptions::new()
-        .create(true)
-        .write(true)
-        .append(true)
-        .open(filename)?,
+      file: RefCell::new(
+        OpenOptions::new()
+          .create(true)
+          .write(true)
+          .append(true)
+          .open(filename)?,
+      ),
     }))
   }
 }
 
 impl Peer for FilePeer {
-  fn write(&mut self, data: Data) -> Result<()> {
-    self.file.write_all(&data)?;
-    Ok(self.file.sync_data()?)
+  fn write(&self, data: Bytes) -> Result<()> {
+    self.file.borrow_mut().write_all(&data)?;
+    Ok(self.file.borrow_mut().sync_data()?)
   }
   fn id(&self) -> i32 {
     self.id
