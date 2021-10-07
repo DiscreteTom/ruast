d7a3f36be8487818c9aaa780dab94dede1613b07
DiscreteTom <discrete_tom@outlook.com>
2021-10-07 21:49:07 +0800

    Peer.write need mut self

--- rua/src/model.rs
+++ rua/src/model.rs
@@ -5,7 +5,7 @@ pub type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 pub type MultiResult<T> = HashMap<i32, Result<T>>;
 
 pub trait Peer {
-  fn write(&self, data: Bytes) -> Result<()>;
+  fn write(&mut self, data: Bytes) -> Result<()>;
   fn id(&self) -> i32;
   fn set_tag(&mut self, tag: &str);
   fn tag(&self) -> &str;
