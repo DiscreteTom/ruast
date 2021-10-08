ea6fa874849007764fe790e0a54cc2691526a4b0
DiscreteTom <discrete_tom@outlook.com>
2021-10-09 00:13:54 +0800

    update examples/file-persistent

--- rua/examples/file-persistent.rs
+++ rua/examples/file-persistent.rs
@@ -6,16 +6,17 @@ use rua::{
   peer::{FilePeer, StdioPeer},
 };
 
-fn main() -> Result<(), Box<dyn Error>> {
-  let h = EventHub::new();
+#[tokio::main]
+pub async fn main() -> Result<(), Box<dyn Error>> {
+  let mut h = EventHub::new(256);
 
-  h.add_peer(StdioPeer::new(0, h.tx()))?;
-  h.add_peer(FilePeer::new(1, "log.txt")?)?;
+  h.add_peer(StdioPeer::new(0, h.tx(), 256))?;
+  h.add_peer(FilePeer::new(1, "log.txt", 256)?)?;
 
   loop {
-    match h.recv() {
+    match h.recv().await {
       Event::PeerMsg(msg) => {
-        h.broadcast_all(msg.data);
+        h.broadcast_all(msg.data).await;
       }
       _ => break,
     }
