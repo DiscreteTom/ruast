97d6a1dd4519a0b684369855f90404f5be9de1f7
DiscreteTom <discrete_tom@outlook.com>
2021-10-09 01:02:04 +0800

    update example/echo/lockstep to use tokio

--- rua/examples/echo-lockstep.rs
+++ rua/examples/echo-lockstep.rs
@@ -1,34 +1,36 @@
-use std::sync::Arc;
-
+use bytes::Bytes;
 use rua::{
   controller::{EventHub, LockstepController},
   model::HubEvent,
   peer::StdioPeer,
 };
 
-fn main() {
+#[tokio::main]
+pub async fn main() {
   let mut peer_msgs = Vec::new();
-  let h = EventHub::new();
+  let mut h = EventHub::new(256);
 
   let lockstep_op_code = 0;
   let mut lockstepper = LockstepController::new(1000, h.tx_clone(), lockstep_op_code);
   lockstepper.next_step();
 
-  h.add_peer(StdioPeer::new(0, h.tx_clone())).unwrap();
+  h.add_peer(StdioPeer::new(0, h.tx_clone(), 256).build())
+    .unwrap();
 
   loop {
-    match h.recv() {
+    match h.recv().await {
       HubEvent::PeerMsg(msg) => peer_msgs.push(msg.data),
       HubEvent::Custom(code) => {
         if code == lockstep_op_code {
           // write current step
-          h.broadcast_all(Arc::new(
+          h.broadcast_all(Bytes::from(
             (lockstepper.current_step().to_string() + ":\n").into_bytes(),
-          ));
+          ))
+          .await;
 
           // write all msg
           for data in &peer_msgs {
-            h.broadcast_all(data.clone());
+            h.broadcast_all(data.clone()).await;
           }
 
           peer_msgs.clear();
