ba196d876c90d60fe29d08078daa87b4a7ef6a64
DiscreteTom <discrete_tom@outlook.com>
2021-10-09 22:41:41 +0800

    finish tungstenite/examples/echo-ws

--- plugin/network/tungstenite/examples/echo-ws.rs
+++ plugin/network/tungstenite/examples/echo-ws.rs
@@ -1,33 +1,44 @@
-use std::{sync::mpsc, thread};
-
-use rua::{controller::EventHub, model::Event, peer::StdioPeer};
+use rua::{
+  controller::EventHub,
+  model::{HubEvent, Result},
+  peer::StdioPeerBuilder,
+};
 use rua_tungstenite::listener::WebsocketListener;
 
-fn main() {
+#[tokio::main]
+pub async fn main() -> Result<()> {
   let mut current_peer_id = 0;
 
-  let h = EventHub::new();
-  h.add_peer(StdioPeer::new(current_peer_id, h.tx())).unwrap();
+  let mut h = EventHub::new(256);
+  h.add_peer(StdioPeerBuilder::new(current_peer_id, h.tx_clone(), 256).build())?;
   current_peer_id += 1;
 
   let ws_listener_code = 0;
-  let (peer_tx, peer_rx) = mpsc::channel();
-  let ws_listener = WebsocketListener::new("127.0.0.1:8080", ws_listener_code, h.tx(), peer_tx);
-  thread::spawn(move || ws_listener.start());
+  let (ws_listener, mut ws_peer_rx) =
+    WebsocketListener::new("127.0.0.1:8080", ws_listener_code, h.tx_clone(), 256);
+  tokio::spawn(async move { ws_listener.start().await });
 
   loop {
-    match h.recv() {
-      Event::PeerMsg(msg) => {
-        h.broadcast_all(msg.data);
+    match h.recv().await {
+      HubEvent::PeerMsg(msg) => {
+        h.broadcast_all(msg.data).await;
       }
-      Event::Custom(code) => {
+      HubEvent::Custom(code) => {
         if code == ws_listener_code {
-          h.add_peer(peer_rx.recv().unwrap().build(current_peer_id, h.tx()))
-            .unwrap();
+          h.add_peer(
+            ws_peer_rx
+              .recv()
+              .await
+              .unwrap()
+              .build(current_peer_id, h.tx_clone(), 256)
+              .await,
+          )?;
           current_peer_id += 1;
         }
       }
       _ => break,
     }
   }
+
+  Ok(())
 }
