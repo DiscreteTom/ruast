173c059e99d7ca24c563c85cfab5b65ac584723b
DiscreteTom <discrete_tom@outlook.com>
2021-10-09 21:22:20 +0800

    apply ActivePeer to FilePeer

--- rua/src/peer/persistent/file.rs
+++ rua/src/peer/persistent/file.rs
@@ -1,70 +1,97 @@
 use tokio::{
   io::AsyncWriteExt,
-  sync::mpsc::{self, Receiver, Sender},
+  sync::mpsc::{self, Sender},
 };
 
-use crate::model::{Peer, PeerEvent, Result};
+use crate::model::{ActivePeer, Peer, PeerEvent};
 
 pub struct FilePeer {
   tag: String,
   id: i32,
   filename: String,
-  tx: Sender<PeerEvent>,
-  rx: Option<Receiver<PeerEvent>>,
+  buffer: usize,
 }
 
 impl FilePeer {
-  pub fn new(id: i32, filename: &str, buffer: usize) -> Result<Box<dyn Peer>> {
-    let (tx, rx) = mpsc::channel(buffer);
-    Ok(Box::new(FilePeer {
+  pub fn new(id: i32, filename: String, buffer: usize) -> Self {
+    FilePeer {
       tag: String::from("file"),
       id,
-      tx,
-      rx: Some(rx),
-      filename: String::from(filename),
-    }))
+      filename,
+      buffer,
+    }
+  }
+
+  pub fn with_tag(&mut self, tag: String) -> &Self {
+    self.tag = tag;
+    self
+  }
+
+  pub fn boxed(self) -> Box<dyn Peer> {
+    Box::new(self)
   }
 }
 
 impl Peer for FilePeer {
+  fn id(&self) -> i32 {
+    self.id
+  }
+
+  fn start(self) -> Box<dyn ActivePeer> {
+    Box::new(FileActivePeer::new(
+      self.id,
+      self.tag,
+      self.filename,
+      self.buffer,
+    ))
+  }
+}
+
+pub struct FileActivePeer {
+  tag: String,
+  id: i32,
+  tx: Sender<PeerEvent>,
+}
+
+impl FileActivePeer {
+  pub fn new(id: i32, tag: String, filename: String, buffer: usize) -> Self {
+    let (tx, mut rx) = mpsc::channel(buffer);
+
+    // writer thread
+    tokio::spawn(async move {
+      let mut file = tokio::fs::OpenOptions::new()
+        .create(true)
+        .write(true)
+        .append(true)
+        .open(filename)
+        .await
+        .unwrap();
+      loop {
+        match rx.recv().await.unwrap() {
+          PeerEvent::Write(data) => {
+            file.write_all(&data).await.unwrap();
+            file.sync_data().await.unwrap();
+          }
+          PeerEvent::Stop => break,
+        }
+      }
+    });
+
+    FileActivePeer { id, tag, tx }
+  }
+}
+
+impl ActivePeer for FileActivePeer {
   fn tx(&self) -> &Sender<PeerEvent> {
     &self.tx
   }
   fn id(&self) -> i32 {
     self.id
   }
-  fn set_tag(&mut self, tag: &str) {
-    self.tag = String::from(tag);
+  fn set_tag(&mut self, tag: String) {
+    self.tag = tag;
   }
-
   fn tag(&self) -> &str {
     &self.tag
-  }
-
-  fn start(&mut self) -> Result<()> {
-    let filename = self.filename.clone();
-    if let Some(mut rx) = self.rx.take() {
-      tokio::spawn(async move {
-        let mut file = tokio::fs::OpenOptions::new()
-          .create(true)
-          .write(true)
-          .append(true)
-          .open(filename)
-          .await
-          .unwrap();
-        loop {
-          match rx.recv().await.unwrap() {
-            PeerEvent::Write(data) => {
-              file.write_all(&data).await.unwrap();
-              file.sync_data().await.unwrap();
-            }
-            PeerEvent::Stop => break,
-          }
-        }
-      });
-    } else {
-      panic!("stdio error")
-    };
-    Ok(())
   }
 }
