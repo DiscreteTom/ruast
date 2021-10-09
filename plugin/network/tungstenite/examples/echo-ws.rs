0b059a2cdfeae22edb719fdd0afd43cf4a6caebc
DiscreteTom <discrete_tom@outlook.com>
2021-10-09 22:51:07 +0800

    optimize code

--- plugin/network/tungstenite/examples/echo-ws.rs
+++ plugin/network/tungstenite/examples/echo-ws.rs
@@ -5,6 +5,8 @@ use rua::{
 };
 use rua_tungstenite::listener::WebsocketListener;
 
+const WS_LISTENER_ADDR: &str = "localhost:8080";
+
 #[tokio::main]
 pub async fn main() -> Result<()> {
   let mut current_peer_id = 0;
@@ -15,8 +17,10 @@ pub async fn main() -> Result<()> {
 
   let ws_listener_code = 0;
   let (ws_listener, mut ws_peer_rx) =
-    WebsocketListener::new("127.0.0.1:8080", ws_listener_code, h.tx_clone(), 256);
+    WebsocketListener::new(WS_LISTENER_ADDR, ws_listener_code, h.tx_clone(), 256);
   tokio::spawn(async move { ws_listener.start().await });
+
+  println!("WebSocket listener is running at ws://{}", WS_LISTENER_ADDR);
 
   loop {
     match h.recv().await {
