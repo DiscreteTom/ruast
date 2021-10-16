5ce501db7275a09613fd5cd407e17f720e018d71
DiscreteTom <discrete_tom@outlook.com>
2021-10-17 01:04:06 +0800

    StdioPeerBuilder use async build

--- rua/src/peer/debug/stdio.rs
+++ rua/src/peer/debug/stdio.rs
@@ -1,7 +1,10 @@
 use async_trait::async_trait;
 use bytes::Bytes;
-use std::io::{self, Stdout, Write};
-use tokio::sync::mpsc::Sender;
+use std::sync::Arc;
+use tokio::{
+  io::{self, AsyncWriteExt, Stdout},
+  sync::{mpsc::Sender, Mutex},
+};
 
 use crate::{
   impl_peer, impl_peer_builder,
@@ -37,58 +40,45 @@ impl StdioPeerBuilder {
   }
 }
 
+#[async_trait]
 impl PeerBuilder for StdioPeerBuilder {
   impl_peer_builder!(all);
 
-  fn build(&mut self) -> Result<Box<dyn Peer>> {
-    Ok(Box::new(StdioPeer::new(
-      self.id.expect("id is required to build StdioPeer"),
-      self.tag.clone(),
-      self
-        .hub_tx
-        .as_ref()
-        .expect("hub_tx is required to build StdioPeer")
-        .clone(),
-      self.buffer.expect("buffer is required to build StdioPeer"),
-      self.disable_input,
-    )))
-  }
-}
+  async fn build(&mut self) -> Result<Box<dyn Peer>> {
+    let id = self.id.expect("id is required to build StdioPeer");
+    let tag = self.tag.clone();
+    let hub_tx = self
+      .hub_tx
+      .as_ref()
+      .expect("hub_tx is required to build StdioPeer")
+      .clone();
+    let buffer = self.buffer.expect("buffer is required to build StdioPeer");
+    let running = Arc::new(Mutex::new(true));
 
-pub struct StdioPeer {
-  tag: String,
-  id: u32,
-  stdout: Stdout,
-}
-
-impl StdioPeer {
-  fn new(
-    id: u32,
-    tag: String,
-    hub_tx: Sender<HubEvent>,
-    buffer: usize,
-    disable_input: bool,
-  ) -> Self {
     // start reader thread
-    if !disable_input {
+    if !self.disable_input {
       tokio::spawn(async move {
-        let stdin = io::stdin();
+        let stdin = std::io::stdin();
         loop {
           // read line
           let mut line = String::new();
           match stdin.read_line(&mut line) {
             Ok(0) => break, // EOF
             Ok(_) => {
-              // remove tail '\n'
-              line.pop();
-              // send
-              hub_tx
-                .send(HubEvent::PeerMsg(PeerMsg {
-                  peer_id: id,
-                  data: Bytes::from(line.into_bytes()),
-                }))
-                .await
-                .unwrap()
+              if *running.lock().await {
+                // remove tail '\n'
+                line.pop();
+                // send
+                hub_tx
+                  .send(HubEvent::PeerMsg(PeerMsg {
+                    peer_id: id,
+                    data: Bytes::from(line.into_bytes()),
+                  }))
+                  .await
+                  .unwrap()
+              } else {
+                break;
+              }
             }
             Err(_) => break,
           }
@@ -96,12 +86,20 @@ impl StdioPeer {
       });
     }
 
-    Self {
-      tag,
+    Ok(Box::new(StdioPeer {
       id,
+      tag,
       stdout: io::stdout(),
-    }
+      running: running.clone(),
+    }))
   }
+}
+
+pub struct StdioPeer {
+  tag: String,
+  id: u32,
+  stdout: Stdout,
+  running: Arc<Mutex<bool>>,
 }
 
 #[async_trait]
@@ -109,9 +107,15 @@ impl Peer for StdioPeer {
   impl_peer!(all);
 
   async fn write(&self, data: Bytes) -> Result<()> {
+    self.stdout.write_all(&data.to_vec()).await?;
     println!("{}", String::from_utf8_lossy(&data));
-    Ok(self.stdout.flush()?)
+    Ok(self.stdout.flush().await?)
   }
 
-  fn stop(&mut self) {}
+  fn stop(&mut self) {
+    let running = self.running.clone();
+    tokio::spawn(async move {
+      *running.lock().await = false;
+    });
+  }
 }
