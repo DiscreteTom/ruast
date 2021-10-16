4ca7c4414480f433de5f4a8f3a18a6de5b910458
DiscreteTom <discrete_tom@outlook.com>
2021-10-16 20:16:42 +0800

    move peer macros to peer/utils

--- rua/src/peer/mod.rs
+++ rua/src/peer/mod.rs
@@ -1,28 +1,5 @@
 pub mod debug;
+pub mod utils;
 pub use debug::stdio::StdioPeerBuilder;
 pub mod persistent;
 pub use persistent::file::FilePeerBuilder;
-
-#[macro_export]
-macro_rules! impl_peer {
-  (id) => {
-    fn id(&self) -> u32 {
-      self.id
-    }
-  };
-  (set_tag) => {
-    fn set_tag(&mut self, tag: String) {
-      self.tag = tag;
-    }
-  };
-  (tag) => {
-    fn tag(&self) -> &str {
-      &self.tag
-    }
-  };
-  (all) => {
-    impl_peer!(id);
-    impl_peer!(set_tag);
-    impl_peer!(tag);
-  };
-}
---
+++ rua/src/peer/utils.rs
@@ -0,0 +1,23 @@
+#[macro_export]
+macro_rules! impl_peer {
+  (id) => {
+    fn id(&self) -> u32 {
+      self.id
+    }
+  };
+  (set_tag) => {
+    fn set_tag(&mut self, tag: String) {
+      self.tag = tag;
+    }
+  };
+  (tag) => {
+    fn tag(&self) -> &str {
+      &self.tag
+    }
+  };
+  (all) => {
+    impl_peer!(id);
+    impl_peer!(set_tag);
+    impl_peer!(tag);
+  };
+}
