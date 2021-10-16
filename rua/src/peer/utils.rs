76d4c73559b51606c9f248c66a82d32fd576b995
DiscreteTom <discrete_tom@outlook.com>
2021-10-17 00:14:46 +0800

    add impl_peer_builder! & comments

--- rua/src/peer/utils.rs
+++ rua/src/peer/utils.rs
@@ -1,3 +1,22 @@
+/// Easily implement the Peer trait.
+///
+/// # Examples
+///
+/// ```
+/// pub struct MyPeer{};
+///
+/// impl Peer for MyPeer {
+///   // implement id(), set_tag(), tag()
+///   impl_peer!(all);
+///   
+///   // or implement selected methods
+///   // impl_peer!(id, tag);
+///
+///   // there are still some methods you need to implement manually.
+///   async fn write(&self, data: Bytes) -> Result<()> { Ok(()) }
+///   fn stop(&mut self){}
+/// }
+/// ```
 #[macro_export]
 macro_rules! impl_peer {
   (id) => {
@@ -16,8 +35,74 @@ macro_rules! impl_peer {
     }
   };
   (all) => {
-    impl_peer!(id);
-    impl_peer!(set_tag);
-    impl_peer!(tag);
+    impl_peer!(id, set_tag, tag);
   };
+  ($($i:ident),+) =>{
+    $(impl_peer!($i);)*
+  };
+}
+
+/// Easily implement the PeerBuilder trait.
+///
+/// # Examples
+///
+/// ```
+/// pub struct MyPeerBuilder{};
+///
+/// impl PeerBuilder for MyPeerBuilder {
+///   // implement id(), hub_tx(), tag(), buffer(), get_id(), get_tag()
+///   impl_peer_builder!(all);
+///   
+///   // or implement selected methods
+///   // impl_peer_builder!(id, tag);
+///
+///   // there are still some methods you need to implement manually.
+///   fn build(&mut self) -> Result<Box<dyn Peer>> { Ok(...) }
+/// }
+/// ```
+#[macro_export]
+macro_rules! impl_peer_builder {
+  (id) => {
+    fn id(&mut self, id: u32) -> &mut dyn PeerBuilder {
+      self.id = Some(id);
+      self
+    }
+  };
+
+  (hub_tx) => {
+    fn hub_tx(&mut self, tx: Sender<HubEvent>) -> &mut dyn PeerBuilder {
+      self.hub_tx = Some(tx);
+      self
+    }
+  };
+
+  (tag) => {
+    fn tag(&mut self, tag: String) -> &mut dyn PeerBuilder {
+      self.tag = tag;
+      self
+    }
+  };
+
+  (buffer) => {
+    fn buffer(&mut self, buffer: usize) -> &mut dyn PeerBuilder {
+      self.buffer = Some(buffer);
+      self
+    }
+  };
+  (get_id) => {
+    fn get_id(&self) -> Option<u32> {
+      self.id
+    }
+  };
+  (get_tag) => {
+    fn get_tag(&self) -> &str {
+      &self.tag
+    }
+  };
+  (all) => {
+    impl_peer_builder!(id,hub_tx,tag,buffer,get_id,get_tag);
+  };
+  ($($i:ident),+)=>{
+    $(impl_peer_builder!($i);)*
+  }
 }
