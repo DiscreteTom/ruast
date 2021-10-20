c9da2b712b3ccbb98f3e23c5dd317e1ecc1efbbc
DiscreteTom <discrete_tom@outlook.com>
2021-10-21 00:52:29 +0800

    update peer_builder util macro

--- rua/src/peer/utils.rs
+++ rua/src/peer/utils.rs
@@ -20,30 +20,30 @@
 ///   fn stop(&mut self){}
 /// }
 /// ```
-#[macro_export]
-macro_rules! impl_peer {
-  (id) => {
-    fn id(&self) -> u32 {
-      self.id
-    }
-  };
-  (set_tag) => {
-    fn set_tag(&mut self, tag: String) {
-      self.tag = tag;
-    }
-  };
-  (tag) => {
-    fn tag(&self) -> &str {
-      &self.tag
-    }
-  };
-  (all) => {
-    impl_peer!(id, set_tag, tag);
-  };
-  ($($i:ident),+) =>{
-    $(impl_peer!($i);)*
-  };
-}
+// #[macro_export]
+// macro_rules! impl_peer {
+//   (id) => {
+//     fn id(&self) -> u32 {
+//       self.id
+//     }
+//   };
+//   (set_tag) => {
+//     fn set_tag(&mut self, tag: String) {
+//       self.tag = tag;
+//     }
+//   };
+//   (tag) => {
+//     fn tag(&self) -> &str {
+//       &self.tag
+//     }
+//   };
+//   (all) => {
+//     impl_peer!(id, set_tag, tag);
+//   };
+//   ($($i:ident),+) =>{
+//     $(impl_peer!($i);)*
+//   };
+// }
 
 /// Easily implement the PeerBuilder trait.
 ///
@@ -66,40 +66,75 @@ macro_rules! impl_peer {
 ///   async fn build(&mut self) -> Result<Box<dyn Peer>> { Ok(...) }
 /// }
 /// ```
+// #[macro_export]
+// macro_rules! impl_peer_builder {
+//   (id) => {
+//     fn id(&mut self, id: u32) -> &mut dyn PeerBuilder {
+//       self.id = Some(id);
+//       self
+//     }
+//   };
+
+//   (server_tx) => {
+//     fn server_tx(&mut self, tx: Sender<ServerEvent>) -> &mut dyn PeerBuilder {
+//       self.server_tx = Some(tx);
+//       self
+//     }
+//   };
+
+//   (tag) => {
+//     fn tag(&mut self, tag: String) -> &mut dyn PeerBuilder {
+//       self.tag = Some(tag);
+//       self
+//     }
+//   };
+//   (get_id) => {
+//     fn get_id(&self) -> Option<u32> {
+//       self.id
+//     }
+//   };
+//   (get_tag) => {
+//     fn get_tag(&self) -> &str {
+//       &self.tag.as_ref().unwrap()
+//     }
+//   };
+//   (all) => {
+//     impl_peer_builder!(id,server_tx,tag,get_id,get_tag);
+//   };
+//   ($($i:ident),+)=>{
+//     $(impl_peer_builder!($i);)*
+//   }
+// }
+
 #[macro_export]
 macro_rules! impl_peer_builder {
   (id) => {
-    fn id(&mut self, id: u32) -> &mut dyn PeerBuilder {
+    pub fn id(&mut self, id: u32) -> &mut Self {
       self.id = Some(id);
       self
     }
   };
 
-  (server_tx) => {
-    fn server_tx(&mut self, tx: Sender<ServerEvent>) -> &mut dyn PeerBuilder {
-      self.server_tx = Some(tx);
+  (sink) => {
+    pub fn sink(&mut self, sink: Sender<PeerEvent>) -> &mut Self {
+      self.sink = Some(sink);
       self
     }
   };
 
   (tag) => {
-    fn tag(&mut self, tag: String) -> &mut dyn PeerBuilder {
-      self.tag = Some(tag);
+    pub fn tag(&mut self, tag: String) -> &mut Self {
+      self.tag = tag;
       self
     }
   };
-  (get_id) => {
-    fn get_id(&self) -> Option<u32> {
-      self.id
-    }
-  };
-  (get_tag) => {
-    fn get_tag(&self) -> &str {
-      &self.tag.as_ref().unwrap()
+  (tx) => {
+    pub fn tx(&self) -> &Sender<PeerEvent> {
+      &self.tx
     }
   };
   (all) => {
-    impl_peer_builder!(id,server_tx,tag,get_id,get_tag);
+    impl_peer_builder!(id,tx,tag,sink);
   };
   ($($i:ident),+)=>{
     $(impl_peer_builder!($i);)*
