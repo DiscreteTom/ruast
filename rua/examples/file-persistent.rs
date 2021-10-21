e52f59973e114587cb65295a55a22867173836b4
DiscreteTom <discrete_tom@outlook.com>
2021-10-21 22:09:20 +0800

    update example/file-persistent

--- rua/examples/file-persistent.rs
+++ rua/examples/file-persistent.rs
@@ -1,28 +1,39 @@
 use rua::model::{PeerEvent, Result};
-use rua::peer::persistent::file::FilePeerBuilder;
-use rua::{broadcaster::Broadcaster, peer::StdioPeerBuilder};
+use rua::peer::FilePeer;
+use rua::{broadcaster::Broadcaster, peer::StdioPeer};
 
 #[tokio::main]
 pub async fn main() -> Result<()> {
+  // Create a new broadcaster
   let mut bc = Broadcaster::new(16);
 
+  // Add StdioPeer to Broadcaster
   bc.add_target({
-    let mut builder = StdioPeerBuilder::new(16);
-    builder.id(0).sink(bc.tx().clone());
-    builder.build().await.unwrap()
+    let mut stdio = StdioPeer::new(16);
+    let tx = stdio.tx().clone();
+    stdio.sink(bc.tx().clone()); // send to broadcaster
+    stdio.spawn(); // start StdioPeer
+    tx
   })
   .await;
 
+  // Add FilePeer to Broadcaster
   bc.add_target({
-    let mut builder = FilePeerBuilder::new(16);
-    builder.id(1).filename("log.txt".to_string());
-    builder.build().await.unwrap()
+    let mut file = FilePeer::new(16);
+    let tx = file.tx().clone();
+    file.filename("log.txt".to_string());
+    file.spawn().await.expect("build FilePeer failed");
+    tx
   })
   .await;
 
+  // Wait for Ctrl-C
   tokio::signal::ctrl_c().await.unwrap();
 
+  // Broadcast `PeerEvent::Stop`
   bc.tx().send(PeerEvent::Stop).await.ok();
+
+  // Stop Broadcaster
   bc.stop().await;
 
   Ok(())
