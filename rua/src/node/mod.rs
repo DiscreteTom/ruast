045a911af3fcc4ae51496a520883c678b29056b1
DiscreteTom <discrete_tom@outlook.com>
2021-10-22 12:18:54 +0800

    update mod.rs

--- rua/src/node/mod.rs
+++ rua/src/node/mod.rs
@@ -3,7 +3,7 @@ pub use broadcaster::Broadcaster;
 pub mod file;
 pub use file::FileNode;
 pub mod lockstep;
-pub use lockstep::Dlc;
+pub use lockstep::{Dlc, Lc};
 pub mod stdio;
 pub use stdio::StdioNode;
 pub mod utils;
