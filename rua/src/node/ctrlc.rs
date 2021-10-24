500a85360a876339a15b9b1a980beb17e72b8388
DiscreteTom <discrete_tom@outlook.com>
2021-10-24 17:45:46 +0800

    apply pub/sub to CtrlC

--- rua/src/node/ctrlc.rs
+++ rua/src/node/ctrlc.rs
@@ -1,7 +1,6 @@
-use crate::{
-  impl_node,
-  model::{NodeEvent, Result, Tx},
-};
+use tokio::sync::broadcast::channel;
+
+use crate::model::{Btx, NodeEvent};
 
 /// Ctrl-C handler.
 ///
@@ -16,42 +15,45 @@ use crate::{
 /// ## Async wait and send NodeEvent
 ///
 /// ```
-/// let (tx, rx) = tokio::mpsc::channel(1);
-/// Ctrlc::new().sink(tx).spawn().unwrap();
+/// let (tx, rx) = tokio::broadcast::channel(1);
+/// Ctrlc::new().publish(tx).spawn().unwrap();
 /// rx.recv().await;
 /// ```
 ///
 /// ## Wait for Ctrl-C then send NodeEvent
 ///
 /// ```
-/// let (tx, rx) = tokio::mpsc::channel(1);
-/// Ctrlc::new().sink(tx).wait().await;
+/// let (tx, rx) = tokio::broadcast::channel(1);
+/// Ctrlc::new().publish(tx).wait().await;
 /// rx.recv().await;
 /// ```
 pub struct Ctrlc {
-  sink: Option<Tx>,
+  btx: Btx,
 }
 
 impl Ctrlc {
-  impl_node!(sink);
-
   pub fn new() -> Self {
-    Self { sink: None }
+    let (btx, _) = channel(1);
+    Self { btx }
   }
 
-  /// You have to set `sink` before `spawn`.
-  pub fn spawn(self) -> Result<()> {
-    let sink = self.sink.ok_or("missing sink when spawn ctrlc")?;
+  pub fn publish(self, target: Btx) -> Self {
+    let mut brx = self.btx.subscribe();
+    tokio::spawn(async move {
+      let e = brx.recv().await.unwrap(); // e is NodeEvent::Stop
+      target.send(e).ok();
+    });
+    self
+  }
+
+  pub fn spawn(self) {
+    let btx = self.btx;
     tokio::spawn(async move {
       tokio::signal::ctrl_c()
         .await
         .expect("failed to listen for ctrlc");
-      sink
-        .send(NodeEvent::Stop)
-        .await
-        .expect("ctrlc failed to send NodeEvent::Stop");
+      btx.send(NodeEvent::Stop).ok();
     });
-    Ok(())
   }
 
   /// Wait for Ctrl-C. If `sink` exists, send `NodeEvent::Stop` to the sink.
@@ -60,11 +62,6 @@ impl Ctrlc {
       .await
       .expect("failed to listen for ctrlc");
 
-    if let Some(sink) = self.sink {
-      sink
-        .send(NodeEvent::Stop)
-        .await
-        .expect("ctrlc failed to send NodeEvent::Stop");
-    }
+    self.btx.send(NodeEvent::Stop).ok();
   }
 }
