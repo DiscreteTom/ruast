16efea7df9be2fc69ea3fc4c1e4f3e2bda2e383d
DiscreteTom <discrete_tom@outlook.com>
2021-10-25 01:18:29 +0800

    add DlsNode

--- rua/src/node/lockstep.rs
+++ rua/src/node/lockstep.rs
@@ -2,7 +2,9 @@ use super::mock::MockNode;
 use crate::impl_node;
 use crate::model::{Brx, Btx, NodeEvent, ReaderNode, Result, Rx, Tx, WriterNode};
 use bytes::Bytes;
+use std::sync::Arc;
 use std::time::Duration;
+use tokio::sync::Mutex;
 use tokio::{
   sync::{broadcast, mpsc},
   time::{self, Instant},
@@ -112,5 +114,116 @@ impl<State: Send + 'static> LsNode<State> {
     });
 
     Ok(MockNode::new(self.btx, self.tx))
+  }
+}
+
+/// Dynamic lockstep node.
+pub struct DlsNode<State: Send + 'static> {
+  tx: Tx,
+  rx: Rx,
+  btx: Btx,
+  step_length_ms: Arc<Mutex<u64>>,
+  propagate_stop: bool,
+  state: State,
+  msg_handler: Option<Box<dyn Fn(u64, Bytes, &mut State) + 'static + Send>>,
+  step_handler: Option<Box<dyn Fn(u64, &mut State) -> Bytes + 'static + Send>>,
+}
+
+impl<State: Send + 'static> ReaderNode for DlsNode<State> {
+  impl_node!(brx);
+}
+
+impl<State: Send + 'static> WriterNode for DlsNode<State> {
+  impl_node!(tx);
+}
+
+impl<State: Send + 'static> DlsNode<State> {
+  impl_node!(publish, subscribe);
+
+  pub fn new(step_length_ms: u64, state: State, write_buffer: usize, read_buffer: usize) -> Self {
+    let (tx, rx) = mpsc::channel(write_buffer);
+    let (btx, _) = broadcast::channel(read_buffer);
+
+    Self {
+      tx,
+      rx,
+      btx,
+      state,
+      step_length_ms: Arc::new(Mutex::new(step_length_ms)),
+      propagate_stop: false,
+      msg_handler: None,
+      step_handler: None,
+    }
+  }
+
+  pub fn default(step_length_ms: u64, state: State) -> Self {
+    Self::new(step_length_ms, state, 16, 16)
+  }
+
+  pub fn propagate_stop(mut self, enable: bool) -> Self {
+    self.propagate_stop = enable;
+    self
+  }
+
+  pub fn on_msg(mut self, f: impl Fn(u64, Bytes, &mut State) + 'static + Send) -> Self {
+    self.msg_handler = Some(Box::new(f));
+    self
+  }
+
+  pub fn on_step(mut self, f: impl Fn(u64, &mut State) -> Bytes + 'static + Send) -> Self {
+    self.step_handler = Some(Box::new(f));
+    self
+  }
+
+  pub fn step_length_ms(&self) -> Arc<Mutex<u64>> {
+    self.step_length_ms.clone()
+  }
+
+  pub fn spawn(self) -> Result<(MockNode, Arc<Mutex<u64>>)> {
+    let reducer = self
+      .step_handler
+      .ok_or("missing reducer when spawn LsNode")?;
+    let mapper = self.msg_handler.ok_or("missing mapper when spawn LsNode")?;
+    let mut state = self.state;
+    let mut rx = self.rx;
+    let btx = self.btx.clone();
+    let step_length_ms = self.step_length_ms.clone();
+    let propagate_stop = self.propagate_stop;
+
+    tokio::spawn(async move {
+      let mut current = 0;
+      let mut timeout = Instant::now() + Duration::from_millis(*step_length_ms.lock().await);
+
+      loop {
+        tokio::select! {
+          e = rx.recv() => {
+            match e {
+              None => break,
+              Some(e) => {
+                match e {
+                  NodeEvent::Stop => {
+                    if propagate_stop {
+                      btx.send(NodeEvent::Stop).unwrap();
+                    }
+                    break
+                  }
+                  NodeEvent::Write(data) => {
+                    (mapper)(current, data, &mut state);
+                  }
+                }
+              }
+            }
+          }
+          _ = time::sleep_until(timeout) => {
+            btx.send(NodeEvent::Write((reducer)(current, &mut state))).unwrap();
+
+            current += 1;
+            timeout += Duration::from_millis(*step_length_ms.lock().await);
+          }
+        }
+      }
+    });
+
+    Ok((MockNode::new(self.btx, self.tx), self.step_length_ms))
   }
 }
