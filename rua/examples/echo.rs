b00d1188c05ae7632f4042d44c4711120ef1e082
DiscreteTom <discrete_tom@outlook.com>
2021-10-24 20:50:28 +0800

    update example/echo

--- rua/examples/echo.rs
+++ rua/examples/echo.rs
@@ -1,17 +1,10 @@
-use rua::{
-  model::Result,
-  node::{ctrlc::Ctrlc, StdioNode},
-};
+use rua::node::{ctrlc::Ctrlc, StdioNode};
 
 #[tokio::main]
-pub async fn main() -> Result<()> {
-  // create & run StdioNode
-  let stdio = StdioNode::new(16)
-    .echo() // set sink to self
-    .spawn(); // spawn reader & writer, return tx for StdioNode
+pub async fn main() {
+  let stdio = StdioNode::default()
+    .echo() // stdin => stdout
+    .spawn();
 
-  // wait for ctrl-c, then stop node
-  Ctrlc::new().sink(stdio).wait().await;
-
-  Ok(())
+  Ctrlc::new().publish(&stdio).wait().await;
 }
