9e0fd918d01a43856841930056d1f1f94f989528
DiscreteTom <discrete_tom@outlook.com>
2021-10-29 21:14:26 +0800

    new model like golang rua

--- rua/src/model.rs
+++ rua/src/model.rs
@@ -1,23 +1,9 @@
 use bytes::Bytes;
-use tokio::sync::{broadcast, mpsc};
+use tokio::sync::mpsc;
 
 pub type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 
-#[derive(Debug, Clone)]
-pub enum NodeEvent {
-  Write(Bytes),
-  Stop,
-}
-
-pub type Tx = mpsc::Sender<NodeEvent>;
-pub type Rx = mpsc::Receiver<NodeEvent>;
-pub type Btx = broadcast::Sender<NodeEvent>;
-pub type Brx = broadcast::Receiver<NodeEvent>;
-
-pub trait WriterNode {
-  fn tx(&self) -> &Tx;
-}
-
-pub trait ReaderNode {
-  fn brx(&self) -> Brx;
-}
+pub type Tx = mpsc::Sender<Bytes>;
+pub type Rx = mpsc::Receiver<Bytes>;
+pub type Utx = mpsc::Sender<()>;
+pub type Urx = mpsc::Receiver<()>;
