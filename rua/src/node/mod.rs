c213919521a00ed0f8476de5fb1a8082e0347c47
DiscreteTom <discrete_tom@outlook.com>
2021-10-29 23:12:49 +0800

    remove MockNode, update mod.rs

--- rua/src/node/mod.rs
+++ rua/src/node/mod.rs
@@ -1,14 +1,12 @@
-pub mod broadcast;
 pub mod ctrlc;
 pub mod file;
 pub mod lockstep;
-pub mod mock;
+pub mod state;
 pub mod stdio;
 pub mod util;
 
-pub use broadcast::BcNode;
 pub use ctrlc::Ctrlc;
 pub use file::FileNode;
-pub use lockstep::LsNode;
-pub use mock::{MockNode, MockReaderNode, MockWriterNode};
+pub use lockstep::Lockstep;
+pub use state::StateNode;
 pub use stdio::StdioNode;
