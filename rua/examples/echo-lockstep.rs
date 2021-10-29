11c2816d5be139ec4dbb279122cf113f63aee698
DiscreteTom <discrete_tom@outlook.com>
2021-10-29 22:33:38 +0800

    update example/echo-lockstep

--- rua/examples/echo-lockstep.rs
+++ rua/examples/echo-lockstep.rs
@@ -1,28 +1,46 @@
 use bytes::BytesMut;
-use rua::model::Result;
-use rua::node::{Ctrlc, LsNode, StdioNode};
+use rua::node::lockstep::Lockstep;
+use rua::node::state::StateNode;
+use rua::node::{ctrlc::Ctrlc, stdio::StdioNode};
 
 #[tokio::main]
-pub async fn main() -> Result<()> {
-  let stdio = StdioNode::default().spawn();
+pub async fn main() {
+  let state = StateNode::default(BytesMut::new()).spawn();
 
-  let ls = LsNode::default(1000, BytesMut::new())
-    .on_msg(|_, data, state| {
-      state.extend_from_slice(&data);
+  let stdio = StdioNode::default()
+    .on_msg({
+      let state = state.clone();
+      move |msg| state.apply(move |buffer| buffer.extend_from_slice(&msg))
     })
-    .on_step(|current_step, state| {
-      let mut buffer = BytesMut::new();
-      // append current step number
-      buffer.extend_from_slice(&(current_step.to_string() + ":\n").into_bytes());
-      buffer.extend_from_slice(state);
-      state.clear();
-      buffer.freeze()
+    .spawn();
+
+  let ls = Lockstep::new()
+    .step_length_ms(1000)
+    .on_step({
+      let state = state.clone();
+      let stdio = stdio.clone();
+      move |step| {
+        state.apply({
+          let stdio = stdio.clone();
+          move |state| {
+            let mut buffer = BytesMut::new();
+            // append current step number
+            buffer.extend_from_slice(&(step.to_string() + ":\n").into_bytes());
+            buffer.extend_from_slice(state);
+            state.clear();
+            stdio.write(buffer.freeze())
+          }
+        })
+      }
     })
-    .subscribe(&stdio) // stdin => lockstep
-    .publish(&stdio) // lockstep => stdout
-    .spawn()?;
+    .spawn()
+    .expect("failed to start lockstep controller");
 
-  Ctrlc::new().publish(&stdio).publish(&ls).wait().await;
-
-  Ok(())
+  Ctrlc::new()
+    .on_signal(move || {
+      ls.stop();
+      stdio.stop()
+    })
+    .wait()
+    .await;
 }
