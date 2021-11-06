f3b59744207e91c6e295e95d67f481536aaf28b7
DiscreteTom <discrete_tom@outlook.com>
2021-11-06 03:25:59 +0000

    new model: callback

--- rua/src/model.rs
+++ rua/src/model.rs
@@ -1,60 +1,190 @@
-use std::fmt;
+use std::{
+  error::Error,
+  fmt::{self, Display},
+  time::Duration,
+};
 
 use bytes::Bytes;
-use rua_macro::{Stoppable, Writable, WritableStoppable};
-use tokio::sync::mpsc;
+use tokio::{
+  sync::mpsc::{Receiver, Sender},
+  time,
+};
 
-pub type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
+pub type GeneralResult<T> = std::result::Result<T, Box<dyn Error>>;
 
-pub type Tx = mpsc::Sender<Bytes>;
-pub type Rx = mpsc::Receiver<Bytes>;
-pub type Utx = mpsc::Sender<()>;
-pub type Urx = mpsc::Receiver<()>;
+pub type CallbackFn = Box<dyn Fn(GeneralResult<()>) + Send>;
+pub type WriteTx = Sender<WritePayload>;
+pub type WriteRx = Receiver<WritePayload>;
+pub type StopTx = Sender<StopPayload>;
+pub type StopRx = Receiver<StopPayload>;
 
-pub trait Writable {
-  fn write(&self, data: Bytes) -> Result<()>;
+pub struct WritePayload {
+  pub data: Bytes,
+  pub callback: Option<CallbackFn>,
 }
 
-pub trait Stoppable {
-  fn stop(&self);
-}
+impl WritePayload {
+  pub fn new(data: Bytes) -> Self {
+    Self {
+      data,
+      callback: None,
+    }
+  }
 
-pub trait WritableStoppable: Writable + Stoppable {}
-
-#[derive(Clone, Stoppable)]
-pub struct StoppableHandle {
-  stop_tx: Utx,
-}
-
-impl StoppableHandle {
-  pub fn new(stop_tx: Utx) -> Self {
-    Self { stop_tx }
+  pub fn then<F>(mut self, callback: F) -> Self
+  where
+    F: Fn(GeneralResult<()>) + Send + 'static,
+  {
+    self.callback = Some(Box::new(callback));
+    self
   }
 }
 
-#[derive(Clone, Writable, Stoppable, WritableStoppable)]
-pub struct WritableStoppableHandle {
-  tx: Tx,
-  stop_tx: Utx,
+#[derive(Default)]
+pub struct StopPayload {
+  pub callback: Option<CallbackFn>,
 }
 
-impl WritableStoppableHandle {
-  pub fn new(tx: Tx, stop_tx: Utx) -> Self {
-    Self { tx, stop_tx }
-  }
-}
-
-#[derive(Debug)]
-pub enum Error {
-  WriteToClosedChannel,
-}
-
-impl fmt::Display for Error {
-  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-    match self {
-      Error::WriteToClosedChannel => write!(f, "write to closed channel"),
+impl StopPayload {
+  pub fn new<F>(callback: F) -> Self
+  where
+    F: Fn(GeneralResult<()>) + Send + 'static,
+  {
+    Self {
+      callback: Some(Box::new(callback)),
     }
   }
 }
 
-impl std::error::Error for Error {}
+#[derive(Debug, Default)]
+pub struct HandleBuilder {
+  tx: Option<WriteTx>,
+  stop_tx: Option<StopTx>,
+  timeout_ms: Option<u64>,
+}
+
+impl HandleBuilder {
+  pub fn tx(mut self, tx: WriteTx) -> Self {
+    self.tx = Some(tx);
+    self
+  }
+
+  pub fn stop_tx(mut self, tx: StopTx) -> Self {
+    self.stop_tx = Some(tx);
+    self
+  }
+
+  pub fn timeout_ms(mut self, ms: u64) -> Self {
+    self.timeout_ms = Some(ms);
+    self
+  }
+
+  pub fn build(self) -> GeneralResult<Handle> {
+    Ok(Handle {
+      tx: self.tx.ok_or("missing tx when build Handle")?,
+      stop_tx: self.stop_tx.ok_or("missing stop_tx when build handle")?,
+      timeout_ms: self.timeout_ms,
+    })
+  }
+}
+
+#[derive(Clone)]
+pub struct Handle {
+  tx: WriteTx,
+  stop_tx: StopTx,
+  timeout_ms: Option<u64>,
+}
+
+impl Handle {
+  pub fn write(&self, data: Bytes) {
+    if let Some(timeout_ms) = self.timeout_ms {
+      self.timed_write(data, timeout_ms)
+    } else {
+      let tx = self.tx.clone();
+      tokio::spawn(async move { tx.send(WritePayload::new(data)).await.ok() });
+    }
+  }
+
+  pub fn write_then<F>(&self, data: Bytes, callback: F)
+  where
+    F: Fn(GeneralResult<()>) + Send + Clone + Sync + 'static,
+  {
+    if let Some(timeout_ms) = self.timeout_ms {
+      self.timed_write_then(data, timeout_ms, callback)
+    } else {
+      let tx = self.tx.clone();
+      tokio::spawn(async move {
+        if tx
+          .send(WritePayload::new(data).then(callback.clone()))
+          .await
+          .is_err()
+        {
+          callback(Err(Box::new(HandleError::ChannelClosed)));
+        }
+      });
+    }
+  }
+
+  /// Override the default timeout.
+  pub fn timed_write(&self, data: Bytes, timeout_ms: u64) {
+    self.timed_write_then(data, timeout_ms, |_| {})
+  }
+
+  /// Override the default timeout.
+  pub fn timed_write_then<F>(&self, data: Bytes, timeout_ms: u64, callback: F)
+  where
+    F: Fn(GeneralResult<()>) + Send + Clone + Sync + 'static,
+  {
+    let tx = self.tx.clone();
+    tokio::spawn(async move {
+      tokio::select! {
+        result = tx.send(WritePayload::new(data).then(callback.clone())) => {
+          if result.is_err(){
+            callback(Err(Box::new(HandleError::ChannelClosed)));
+          }
+        }
+        _ = time::sleep(Duration::from_millis(timeout_ms)) => {
+          callback(Err(Box::new(HandleError::Timeout)));
+        }
+      }
+    });
+  }
+
+  pub fn stop(self) {
+    let stop_tx = self.stop_tx;
+    tokio::spawn(async move { stop_tx.send(StopPayload::default()).await.ok() });
+  }
+
+  pub fn stop_then<F>(self, callback: F)
+  where
+    F: Fn(GeneralResult<()>) + Send + Clone + Sync + 'static,
+  {
+    let stop_tx = self.stop_tx;
+    tokio::spawn(async move {
+      if stop_tx
+        .send(StopPayload::new(callback.clone()))
+        .await
+        .is_err()
+      {
+        callback(Err(Box::new(HandleError::ChannelClosed)));
+      }
+    });
+  }
+}
+
+#[derive(Debug)]
+pub enum HandleError {
+  ChannelClosed,
+  Timeout,
+}
+
+impl Display for HandleError {
+  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+    match self {
+      HandleError::ChannelClosed => write!(f, "channel closed"),
+      HandleError::Timeout => write!(f, "timeout"),
+    }
+  }
+}
+
+impl Error for HandleError {}
