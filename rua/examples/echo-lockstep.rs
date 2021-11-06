2ad5527a208ab5d163c885d58a264b762184cb82
DiscreteTom <discrete_tom@outlook.com>
2021-11-06 08:22:19 +0000

    update example/echo-lockstep

--- rua/examples/echo-lockstep.rs
+++ rua/examples/echo-lockstep.rs
@@ -1,38 +1,35 @@
 use bytes::BytesMut;
 use clonesure::cc;
-use rua::model::{Stoppable, Writable};
-use rua::node::lockstep::Lockstep;
-use rua::node::state::StateNode;
-use rua::node::{ctrlc::Ctrlc, stdio::StdioNode};
+use rua::node::{ctrlc::Ctrlc, lockstep::Lockstep, state::StateNode, stdio::StdioNode};
 
 #[tokio::main]
 pub async fn main() {
-  let state = StateNode::default(BytesMut::new()).spawn();
+  let state = StateNode::with_state(BytesMut::new()).spawn();
 
   let stdio = StdioNode::default()
-    .on_msg(cc!(|@state, msg| state.apply(move |buffer| buffer.extend_from_slice(&msg))))
+    .on_input(cc!(|@state, msg| state.apply(move |buffer| buffer.extend_from_slice(&msg))))
     .spawn();
 
-  let ls = Lockstep::new()
-    .step_length_ms(1000)
+  let ls = Lockstep::default()
     .on_step(cc!(|@state, @stdio, step| {
       state.apply(cc!(|@stdio, state| {
-        let mut buffer = BytesMut::new();
+        let mut result = BytesMut::new();
         // append current step number
-        buffer.extend_from_slice(&(step.to_string() + ":\n").into_bytes());
-        buffer.extend_from_slice(state);
+        result.extend_from_slice(&(step.to_string() + ":\n").into_bytes());
+        result.extend_from_slice(state);
         state.clear();
-        stdio.write(buffer.freeze()).unwrap()
+        stdio.write(result.freeze())
       }))
     }))
     .spawn()
     .expect("failed to start lockstep controller");
 
-  Ctrlc::new()
+  Ctrlc::default()
     .on_signal(move || {
       ls.stop();
       stdio.stop()
     })
     .wait()
-    .await;
+    .await
+    .expect("failed to listen for ctrlc");
 }
