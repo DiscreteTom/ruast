30948100d0a274ddfefb11f8f24fec9ede4cf063
DiscreteTom <discrete_tom@outlook.com>
2021-11-06 13:05:48 +0000

    apply callback model to broadcast

--- rua/src/node/broadcast.rs
+++ rua/src/node/broadcast.rs
@@ -1,46 +1,56 @@
-use std::sync::Arc;
+use std::{collections::HashMap, sync::Arc};
 
 use bytes::Bytes;
-use tokio::sync::{
-  mpsc::{self, Sender},
-  Mutex,
-};
+use tokio::sync::Mutex;
 
-use crate::model::{GeneralResult, Handle, HandleBuilder, StopTx, WriteRx, WriteTx};
+use crate::model::{GeneralResult, Handle};
 
 #[derive(Clone, Default)]
 pub struct Broadcaster {
   timeout_ms: Option<u64>,
-  targets: Arc<Mutex<Vec<Handle>>>,
-  propagate_stop: bool,
+  targets: Arc<Mutex<HashMap<usize, Handle>>>,
+  keep_dead_targets: bool,
+  current_handle_id: usize,
 }
 
 impl Broadcaster {
-  pub fn propagate_stop(mut self, enable: bool) -> Self {
-    self.propagate_stop = enable;
+  pub fn keep_dead_targets(mut self, enable: bool) -> Self {
+    self.keep_dead_targets = enable;
     self
   }
 
-  pub fn add_target(&self, handle: Handle) {
-    let targets = self.targets.clone();
-    tokio::spawn(async move { targets.lock().await.push(handle) });
+  pub fn add_target(&mut self, handle: Handle) {
+    self.add_target_then(handle, |_| {})
   }
 
-  pub fn add_target_then<F>(&self, handle: Handle, callback: F)
+  pub fn add_target_then<F>(&mut self, handle: Handle, callback: F)
   where
-    F: Fn() + Send + Sync + 'static,
+    F: Fn(usize) + Send + Sync + 'static,
   {
     let targets = self.targets.clone();
+    let id = self.current_handle_id;
+    self.current_handle_id += 1;
     tokio::spawn(async move {
-      targets.lock().await.push(handle);
-      callback()
+      targets.lock().await.insert(id, handle);
+      callback(id)
     });
+  }
+
+  pub fn remove_target(&self, id: usize) {
+    self.remove_target_then(id, |_| {})
+  }
+
+  pub fn remove_target_then<F>(&self, id: usize, callback: F)
+  where
+    F: Fn(Option<Handle>) + Send + Sync + 'static,
+  {
+    let targets = self.targets.clone();
+    tokio::spawn(async move { callback(targets.lock().await.remove(&id)) });
   }
 
   /// Write will be canceled if timeout, in this case you may need to increase the node's buffer.
   pub fn write(&self, data: Bytes) {
-    let targets = self.targets.clone();
-    Self::inner_write(targets, data, self.timeout_ms, |_| {})
+    self.inner_write(data, self.timeout_ms, |_| {})
   }
 
   /// Write will be canceled if timeout, in this case you may need to increase the node's buffer.
@@ -48,15 +58,13 @@ impl Broadcaster {
   where
     F: Fn(GeneralResult<()>) + Send + Clone + Sync + 'static,
   {
-    let targets = self.targets.clone();
-    Self::inner_write(targets, data, self.timeout_ms, callback)
+    self.inner_write(data, self.timeout_ms, callback)
   }
 
   /// Override the default timeout.
   /// Write will be canceled if timeout, in this case you may need to increase the node's buffer.
   pub fn timed_write(&self, data: Bytes, timeout_ms: u64) {
-    let targets = self.targets.clone();
-    Self::inner_write(targets, data, Some(timeout_ms), |_| {})
+    self.inner_write(data, Some(timeout_ms), |_| {})
   }
 
   /// Override the default timeout.
@@ -65,20 +73,32 @@ impl Broadcaster {
   where
     F: Fn(GeneralResult<()>) + Send + Clone + Sync + 'static,
   {
-    let targets = self.targets.clone();
-    Self::inner_write(targets, data, Some(timeout_ms), callback)
+    self.inner_write(data, Some(timeout_ms), callback)
   }
 
-  fn inner_write<F>(
-    targets: Arc<Mutex<Vec<Handle>>>,
-    data: Bytes,
-    timeout_ms: Option<u64>,
-    callback: F,
-  ) where
+  fn inner_write<F>(&self, data: Bytes, timeout_ms: Option<u64>, callback: F)
+  where
     F: Fn(GeneralResult<()>) + Send + Clone + Sync + 'static,
   {
+    let targets = self.targets.clone();
+    let keep_dead_targets = self.keep_dead_targets;
     tokio::spawn(async move {
-      for handle in targets.lock().await.iter() {
+      let targets_locked = targets.lock().await;
+
+      for (id, handle) in targets_locked.iter() {
+        let targets = targets.clone();
+        let keep_dead_targets = keep_dead_targets.clone();
+        let id = *id;
+        let callback = callback.clone();
+
+        let callback = move |result: GeneralResult<()>| {
+          let targets = targets.clone();
+          if result.is_err() && !keep_dead_targets {
+            tokio::spawn(async move { targets.lock().await.remove(&id) });
+          }
+          callback(result);
+        };
+
         if let Some(timeout_ms) = timeout_ms {
           handle.timed_write_then(data.clone(), timeout_ms.clone(), callback.clone());
         } else {
@@ -88,28 +108,23 @@ impl Broadcaster {
     });
   }
 
-  pub fn stop(self) {
-    if self.propagate_stop {
-      let targets = self.targets;
-      tokio::spawn(async move {
-        while let Some(handle) = targets.lock().await.pop() {
-          handle.stop();
-        }
-      });
-    }
+  pub fn stop_all(self) {
+    self.stop_all_then(|_| {})
   }
 
-  pub fn stop_then<F>(self, callback: F)
+  pub fn stop_all_then<F>(self, callback: F)
   where
     F: Fn(GeneralResult<()>) + Send + Clone + Sync + 'static,
   {
-    if self.propagate_stop {
-      let targets = self.targets;
-      tokio::spawn(async move {
-        while let Some(handle) = targets.lock().await.pop() {
+    let targets = self.targets;
+    tokio::spawn(async move {
+      let mut targets = targets.lock().await;
+      let keys: Vec<usize> = targets.keys().map(|x| *x).collect();
+      for id in keys {
+        if let Some(handle) = targets.remove(&id) {
           handle.stop_then(callback.clone());
         }
-      });
-    }
+      }
+    });
   }
 }
