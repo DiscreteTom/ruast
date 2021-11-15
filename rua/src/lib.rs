5f4560a096159a391c6a4f9694c2923cff68503d
DiscreteTom <discrete_tom@outlook.com>
2021-11-15 14:33:55 +0000

    add util macros

--- rua/src/lib.rs
+++ rua/src/lib.rs
@@ -1,2 +1,3 @@
 pub mod model;
 pub mod node;
+pub mod utils;
---
+++ rua/src/utils.rs
@@ -0,0 +1,43 @@
+/// Take members from a struct.
+///
+/// `take(self, value);` will be translated to `let value = self.value;`.
+#[macro_export]
+macro_rules! take {
+  ($self:ident, $($value:ident),*) => {
+    $(
+      let $value = $self.$value;
+    )*
+  };
+}
+
+/// Take option members from a struct.
+///
+/// `take_option(self, value);` will be translated to `let value = self.value.ok_or("missing value")?;`.
+#[macro_export]
+macro_rules! take_option {
+  ($self:ident, $($option:ident),*) => {
+    $(
+      let $option = $self.$option.ok_or(format!("missing {}", stringify!($option)))?;
+    )*
+  };
+}
+
+/// Clone members from a struct.
+///
+/// `clone(self, value);` will be translated to `let value = self.value.clone();`.
+#[macro_export]
+macro_rules! clone {
+  ($self:ident, $($member:ident),*) => {
+    $(
+      let $member = $self.$member.clone();
+    )*
+  };
+}
+
+/// Shorthand for `tokio::spawn`.
+#[macro_export]
+macro_rules! go {
+  ($($body:stmt)*) => {
+    tokio::spawn(async move {$($body)*});
+  };
+}
