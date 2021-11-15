9f5cfa4fc0cdec2c2369c07f1bdcebb39ed5d341
DiscreteTom <discrete_tom@outlook.com>
2021-11-15 16:12:58 +0000

    optimize code

--- rua/examples/tail-file.rs
+++ rua/examples/tail-file.rs
@@ -4,14 +4,17 @@ use rua::node::{ctrlc::Ctrlc, stdio::StdioNode, tail::TailNode};
 #[tokio::main]
 pub async fn main() {
   let stdio = StdioNode::default().spawn();
-  TailNode::with_file_name("log.txt")
+  let tail = TailNode::with_file_name("log.txt")
     .on_new_line(cc!(|@stdio, data| stdio.write(data)))
     .spawn()
     .await
     .expect("failed to tail file");
 
   Ctrlc::default()
-    .on_signal(move || stdio.stop())
+    .on_signal(move || {
+      stdio.stop();
+      tail.stop()
+    })
     .wait()
     .await
     .expect("failed to listen for ctrlc");
