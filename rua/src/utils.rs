3afd478b5c1d85fcf05ae2399b9a9b78787af536
DiscreteTom <discrete_tom@outlook.com>
2021-11-15 14:39:58 +0000

    add take_mut!

--- rua/src/utils.rs
+++ rua/src/utils.rs
@@ -10,6 +10,18 @@ macro_rules! take {
   };
 }
 
+/// Take mutable members from a struct.
+///
+/// `take_mut(self, value);` will be translated to `let mut value = self.value;`.
+#[macro_export]
+macro_rules! take_mut {
+  ($self:ident, $($value:ident),*) => {
+    $(
+      let mut $value = $self.$value;
+    )*
+  };
+}
+
 /// Take option members from a struct.
 ///
 /// `take_option(self, value);` will be translated to `let value = self.value.ok_or("missing value")?;`.
