db2a194677f271850f4268413034cb29380b97d8
DiscreteTom <discrete_tom@outlook.com>
2021-11-16 02:35:54 +0000

    add rua-random as dev-dep

--- rua/examples/tail-file.rs
+++ rua/examples/tail-file.rs
@@ -1,10 +1,25 @@
 use clonesure::cc;
-use rua::node::{ctrlc::Ctrlc, stdio::StdioNode, tail::TailNode};
+use rua::node::{ctrlc::Ctrlc, stdio::StdioNode, tail::TailNode, FileNode};
+use rua_random::RandomNode;
 
 #[tokio::main]
 pub async fn main() {
+  const FILENAME: &str = "log.txt";
+
+  // write file with random bytes
+  let file = FileNode::default()
+    .filename(FILENAME)
+    .spawn()
+    .await
+    .expect("failed to write file");
+  let random = RandomNode::default()
+    .on_msg(cc!(|@file, data| file.write(data)))
+    .spawn()
+    .unwrap();
+
+  // tail file to stdout
   let stdio = StdioNode::default().spawn();
-  let tail = TailNode::with_file_name("log.txt")
+  let tail = TailNode::with_file_name(FILENAME)
     .on_new_line(cc!(|@stdio, data| stdio.write(data)))
     .spawn()
     .await
@@ -13,7 +28,9 @@ pub async fn main() {
   Ctrlc::default()
     .on_signal(move || {
       stdio.stop();
-      tail.stop()
+      tail.stop();
+      random.stop();
+      file.stop();
     })
     .wait()
     .await
