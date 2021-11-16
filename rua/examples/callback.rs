e876fb730f6db943e808b1cfd7a6c98bbae6dde7
DiscreteTom <discrete_tom@outlook.com>
2021-11-16 03:12:27 +0000

    update example/callback

--- rua/examples/callback.rs
+++ rua/examples/callback.rs
@@ -1,6 +1,7 @@
 use bytes::Bytes;
 use clonesure::cc;
 use rua::node::{ctrlc::Ctrlc, file::FileNode, stdio::StdioNode};
+use rua_random::RandomNode;
 
 #[tokio::main]
 pub async fn main() {
@@ -10,25 +11,29 @@ pub async fn main() {
     .await
     .expect("failed to create file peer");
 
-  let stdio_node = StdioNode::default();
-  let stdio = stdio_node.handle().clone();
-  stdio_node
-    .on_input(cc!(|@stdio, @file, msg| {
+  let stdio = StdioNode::default().spawn();
+
+  let random = RandomNode::default()
+    .on_msg(cc!(|@stdio, @file, msg| {
       file.write_then(
         msg,
         cc!(|@stdio, result| {
           match result {
-            Ok(_) => stdio.write(Bytes::from_static(b">> ok")),
-            Err(_) => stdio.write(Bytes::from_static(b">> err")),
+            Ok(_) => stdio.write(Bytes::from_static(b"ok")),
+            Err(_) => stdio.write(Bytes::from_static(b"err")),
           }
         }))
     }))
-    .spawn();
+    .spawn()
+    .unwrap();
 
   Ctrlc::default()
     .on_signal(move || {
-      stdio.stop();
-      file.stop();
+      file.stop_then(cc!(|@random, @stdio, _| {
+        random.clone().stop_then(cc!(|@stdio, _| {
+          stdio.clone().stop();
+        }))
+      }));
     })
     .wait()
     .await
